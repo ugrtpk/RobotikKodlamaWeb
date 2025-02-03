889e2547e12a3a2c5ed3b86724694c562573ca82
ugrtpk <ugurtopak@gmail.com>
2025-02-03 22:12:19 +0300

    Added .gitignore

--- RobotikKodlamaProject/RobotikKodlama.WebUI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace RobotikKodlama.WebUI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
